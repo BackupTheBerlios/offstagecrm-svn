91432d16e45408452941b291bbf0d2381fe242b0
citibob <citibob@877303ed-bb0d-0410-8710-c6465bba135a>
2007-06-12 22:13:33 +0000

    xx

--- trunk/offstage/deploy.sh
+++ trunk/offstage/deploy.sh
@@ -1,2 +1,2 @@
-rsync -avz --dry-run dist/ office@jmbt.merseine.nu:/export/shared/offstage
-
+#rsync -avz dist/ office@jmbt.merseine.nu:/export/shared/offstage
+rsync -avz dist/ root@jmbt.merseine.nu:/Volumes/home/shared/Administration/offstage
