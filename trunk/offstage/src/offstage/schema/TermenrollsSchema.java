e5c96e644e9d28b0b2c875f0d7cd8bf506240ae6
citibob <citibob@877303ed-bb0d-0410-8710-c6465bba135a>
2007-10-22 03:31:27 +0000

    Deployed

---
+++ trunk/offstage/src/offstage/schema/TermenrollsSchema.java
@@ -0,0 +1,46 @@
+/*
+OffstageArts: Enterprise Database for Arts Organizations
+This file Copyright (c) 2005-2007 by Robert Fischer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package offstage.schema;
+
+import citibob.sql.*;
+import citibob.sql.pgsql.*;
+import citibob.jschema.*;
+import citibob.sql.DbChangeModel;
+import java.sql.*;
+import citibob.util.KeyedModel;
+
+public class TermenrollsSchema extends ConstSchema
+{
+
+public TermenrollsSchema(citibob.sql.SqlRunner str, DbChangeModel change)
+throws SQLException
+{
+	super();
+	table = "termenrolls";
+	KeyedModel termids_kmodel = new DbKeyedModel(str, change,
+		"termids", "groupid", "name", "firstdate desc,name");	
+	citibob.util.KeyedModel courseroles_kmodel = new DbKeyedModel(str, change,
+		"courseroles", "courseroleid", "name", "orderid,name");
+	cols = new Column[] {
+		new Column(new SqlEnum(termids_kmodel, false), "groupid", true),
+		new Column(new SqlInteger(false), "entityid", true),
+		new Column(new SqlEnum(courseroles_kmodel, "<none>"), "courserole", false)
+	};
+}
+
+}
