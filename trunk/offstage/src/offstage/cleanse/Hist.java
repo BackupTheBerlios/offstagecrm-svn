dfb20b3acfcdf0b5ca860896e8a8d862116ab4e2
citibob <citibob@877303ed-bb0d-0410-8710-c6465bba135a>
2007-11-04 02:54:21 +0000

    Beginnings of Merge/Purge and Data Cleansing Screen

---
+++ trunk/offstage/src/offstage/cleanse/Hist.java
@@ -0,0 +1,67 @@
+/*
+ * Hist.java
+ *
+ * Created on November 3, 2007, 9:19 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package offstage.cleanse;
+
+import java.util.Arrays;
+import org.apache.commons.collections.ArrayStack;
+
+import java.text.*;
+
+/**
+ *
+ * @author citibob
+ */
+public class Hist
+{
+
+NumberFormat nfmt = new DecimalFormat("#0.00");
+	
+double[] cat;
+int[] count;
+int tot;
+
+/** Creates a new instance of Hist */
+public Hist(double low, double high, int ncat)
+{
+	cat = new double[ncat];
+	double delta = (high - low) / ncat;
+	for (int i=0; i<ncat; ++i) {
+		cat[i] = low + i*delta;
+	}
+	count = new int[ncat];
+}
+
+public void add(double d)
+{
+	int ix = Arrays.binarySearch(cat, d);
+	if (ix >= 0) ++count[ix];
+	else ++count[-(ix+2)];
+	++tot;
+}
+
+public String toString() {
+	StringBuffer sb = new StringBuffer();
+	for (int i=0; i<cat.length; ++i) {
+		sb.append(nfmt.format(cat[i]) + ": " + count[i] + " " +
+			nfmt.format((double)count[i] / (double)tot) + "\n");
+	}
+	return sb.toString();
+}
+public static void main(String[] args)
+{
+	Hist h = new Hist(0,1.0,10);
+	h.add(0);
+	h.add(.1);
+	h.add(.055);
+	h.add(1);
+	h.add(2);
+	System.out.println(h.toString());
+}
+}
---
+++ trunk/offstage/src/offstage/cleanse/MergePurge.java
@@ -0,0 +1,181 @@
+/*
+ * MergePurge.java
+ *
+ * Created on November 3, 2007, 8:39 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package offstage.cleanse;
+
+import citibob.sql.pgsql.*;
+import java.sql.*;
+import java.util.*;
+import citibob.sql.*;
+import com.wcohen.ss.*;
+import com.wcohen.ss.api.*;
+import com.wcohen.ss.tokens.*;
+import java.text.*;
+import offstage.db.*;
+
+/**
+ *
+ * @author citibob
+ */
+public class MergePurge
+{
+
+static NumberFormat nfmt = new DecimalFormat("#0.00");
+
+
+static class Duo implements Comparable
+{
+	public Map.Entry<Integer,StringWrapper> aa,bb;
+	public double score;
+	public Duo(Map.Entry<Integer,StringWrapper> a, Map.Entry<Integer,StringWrapper> b, double score) {
+		this.aa = a;
+		this.bb = b;
+		this.score = score;
+	}
+	public int compareTo(Object o) {
+		Duo d = (Duo)o;
+		double x = (score - d.score);
+		if (x > 0) return 1;
+		if (x < 0) return -1;
+		return 0;
+	}
+	public String toString() {
+		return nfmt.format(score) + " " + aa.getKey() + " " + bb.getKey() + " <" + aa.getValue() + "> <" + bb.getValue() + ">";
+	}
+}
+
+
+static boolean empty(String s) { return (s == null || "".equals(s)); }
+static String upper(String s) { return (s == null ? "" : s.toUpperCase()); }
+
+public String getCanonical(ResultSet rs) throws SQLException
+{
+	StringBuffer sb = new StringBuffer();
+
+	// Get the main address line, not the "c/o" line
+	String address1 = rs.getString("address1");
+	String address2 = rs.getString("address2");
+	String addr = (empty(address2) ? address1 :  address2);
+	String country = rs.getString("country");
+	String zip = rs.getString("zip");
+	if (empty(country) || "USA".equalsIgnoreCase(country)) {
+		addr = AddrTx.AddressLineStandardization(addr);
+		if (zip != null && zip.length() > 5) zip = zip.substring(5);
+	}
+
+	
+	String ret = (addr + " " +
+		upper(rs.getString("city")) + " " +
+		upper(rs.getString("state")) + " " +
+		upper(zip)).trim();
+	return ret;
+//	+ " " +
+//		upper(rs.getString("firstname")) + " " +
+//		upper(rs.getString("lastname")) + " " +
+//		upper(rs.getString("orgname"));
+}
+
+
+void process(Map<Integer,String> xmap, double thresh)
+{
+	
+	// Prepare strings
+	SoftTFIDF fullD = new SoftTFIDF(new SimpleTokenizer(true,true),
+		new JaroWinkler(),0.8);
+	Map<Integer,StringWrapper> map = new TreeMap();
+	for (Map.Entry<Integer,String> aa : xmap.entrySet()) {
+		map.put(aa.getKey(), fullD.prepare(aa.getValue()));
+	}
+	
+	List<Duo> report = new ArrayList();
+	Hist fullHist = new Hist(0,1,10);
+	fullD.train( new BasicStringWrapperIterator(map.values().iterator()));
+System.out.println("Full Processing");
+	int i,j;
+	i=0; j=0;
+	for (Map.Entry<Integer,StringWrapper> aa : map.entrySet()) {
+		if (i % 100 == 0) System.out.println("  " + i);
+		j=0;
+		for (Map.Entry<Integer,StringWrapper> bb : map.entrySet()) {
+			if (j >= i) continue;
+			double e = fullD.score(aa.getValue(), bb.getValue());
+			fullHist.add(e);
+			if (e >= thresh) report.add(new Duo(aa,bb,e));
+			++j;
+		}
+		++i;
+	}
+	
+	// Report
+	Collections.sort(report);
+	for (Duo d : report) {
+		System.out.println(d.toString());
+	}
+	System.out.println(fullHist.toString());
+
+}
+
+
+/** Creates a new instance of MergePurge */
+public MergePurge(SqlRunner str)
+{
+	String sql =
+		" SELECT entityid,primaryentityid," +
+		" address1,address2,city,state,zip,country," +
+		" firstname,lastname,orgname,isorg from persons p" +
+		" where city = 'Cambridge'";
+	str.execSql(sql, new RsRunnable() {
+	public void run(SqlRunner str, ResultSet rs) throws SQLException {
+        // create a SoftTFIDF distance learner
+        SoftTFIDF nameD = new SoftTFIDF(new SimpleTokenizer(true,true),
+			new JaroWinkler(),0.8);
+		
+		Map<Integer,String> nameMap = new TreeMap();
+		Map<Integer,String> addrMap = new TreeMap();
+		Map<Integer,String> orgMap = new TreeMap();
+		int n = 0;
+		while (rs.next()) {
+			// Check for multiple entries at same address
+			int eid = rs.getInt("entityid");
+			int pid = rs.getInt("primaryentityid");
+			if (eid == pid) {
+				String canon = getCanonical(rs);
+				addrMap.put(eid, canon);				
+			}
+
+			// Check for duplicate entries (by name; can catch change of address too)
+			String name = upper(rs.getString("firstname")) + " " + upper(rs.getString("lastname"));
+			name = name.trim();
+			if (!empty(name)) nameMap.put(eid, name);
+			
+			String orgname = upper(rs.getString("orgname")).trim();
+			if (rs.getBoolean("isorg") && !empty(orgname)) orgMap.put(eid, orgname);
+			++n;
+		}
+System.out.println("Done getting names (" + n + " records)");
+		// train the distance on some strings - in general, this would
+		// be a large corpus of existing strings, so that some
+		// meaningful frequency estimates can be accumulated.  for
+		// efficiency, you train on an iterator over StringWrapper
+		// objects, which are produced with the 'prepare' function.
+
+		process(nameMap, .8);
+		process(addrMap, .8);
+		process(orgMap, .8);
+	}});
+}
+public static void main(String[] args) throws Exception
+{
+	citibob.sql.ConnPool pool = offstage.db.DB.newConnPool();
+	SqlBatch str = new SqlBatch();
+	new MergePurge(str);
+	str.exec(pool);
+}
+
+}
